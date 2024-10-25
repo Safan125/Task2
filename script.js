d354091185a99536b5a65c841b19453084f99364
Abdallah Safan <abdallahSafan05@gmail.com>
2024-10-25 23:52:36 +0300

    Stop autoslide hover

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
-let currentIMG=1;
+let currentIMG = 1;
 displayimg(currentIMG);
 
-function displayimg(img){
-var slides=document.getElementsByClassName("slide");
-var dots=document.getElementsByClassName("dot");
-if(img>slides.length)
-    currentIMG=1;
-else if(img<1)
-    currentIMG=slides.length;
-for(var i=0;i<slides.length; i++){
-    slides[i].style.display="none";
-}
-for(var i=0;i<dots.length; i++)
-dots[i].className=dots[i].className.replace(" active"," ");
+function displayimg(img) {
+    const slides = document.getElementsByClassName("slide");
+    const dots = document.getElementsByClassName("dot");
 
-slides[currentIMG - 1].style.display="block";
-dots[currentIMG - 1].className+=" active";
+    if (img > slides.length) currentIMG = 1;
+    else if (img < 1) currentIMG = slides.length;
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[currentIMG - 1].style.display = "block";
+    dots[currentIMG - 1].className += " active";
 }
 
-function currentSlide(n){
-    displayimg(currentIMG=n);
+function currentSlide(n) {
+    displayimg((currentIMG = n));
 }
-function Slide(n){
-    displayimg(currentIMG+=n);
+
+function Slide(n) {
+    displayimg((currentIMG += n));
 }
+
+// Start automatic sliding
+let autoslide = setInterval(() => {
+    Slide(1);
+}, 3000);
+
+// Function to stop the interval
+function stopAutoSlide() {
+    clearInterval(autoslide);
+}
+
+function Auto(){
+    autoslide=setInterval(() => {
+        Slide(1);
+    }, 3000);
+}
+
 // Touch event handling
-const autoslide= setInterval(()=>{
-Slide(1);
-},3000)
-
 let startX = 0;
 
 function handleTouchStart(e) {
@@ -36,14 +51,18 @@ function handleTouchStart(e) {
 }
 
 function handleTouchEnd(e) {
-    let endX = e.changedTouches[0].clientX;
+    const endX = e.changedTouches[0].clientX;
     if (startX - endX > 50) {
-        Slide(1); // Swipe left
+        Slide(1);
     } else if (endX - startX > 50) {
-        Slide(-1); // Swipe right
+        Slide(-1);
     }
 }
 
-// Add touch event listeners to the slider container
+// Add event listeners to stop auto-slide on interaction
 document.querySelector(".container").addEventListener("touchstart", handleTouchStart);
 document.querySelector(".container").addEventListener("touchend", handleTouchEnd);
+document.querySelector(".container").addEventListener("mouseover", stopAutoSlide);
+document.querySelector(".container").addEventListener("touchstart", stopAutoSlide);
+document.querySelector(".container").addEventListener("mouseleave", Auto);
+document.querySelector(".container").addEventListener("touchend", Auto);
